527b208bbb61f9919d0c49f00c7c58d2f8c2f0e7
dayakachlon <96650116+dayakachlon@users.noreply.github.com>
2022-01-06 19:17:58 +0200

    Delete fastparquet_utils.py

--- fastparquet_utils.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-def convert_csvs_to_parquet(symbols_threads):
-    print(symbols_threads)
-    for symbol in symbols_threads:
-        data_frame = pd.read_csv(f"{symbol}.csv")
-        print(data_frame)
-        data_frame.to_parquet(f"{symbol}.parquet")
